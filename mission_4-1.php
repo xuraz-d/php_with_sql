e55583df20f18c693fc8994df37e57b20362d985
xuraz-d <43702078+xuraz-d@users.noreply.github.com>
2018-11-11 13:30:17 +0900

    Update mission_4-1.php

--- mission_4-1.php
+++ mission_4-1.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <title></title>
 </head>
-<body>
+<body bgcolor="black">
 
 <?php
     $e_password =$_POST['password'];
@@ -21,14 +21,24 @@
     
     
     //SQL connection
-    $dsn = 'mysql:dbname=Database_name;host=localhost';
-    $dbusername ="Username";
-    $dbpassword='Password';
-    $tablename = tablename;
+    $dsn = 'mysql:dbname=databaselink/name;host=localhost';
+    $dbusername ="dbusername";
+    $dbpassword='dbpassword';
+    $tablename = 'mission_4_db';
     
     //try catch the error connecting database
     try {
         $pdo = new PDO($dsn,$dbusername,$dbpassword);
+        //create table if doesnot exit
+        $c_sql= "CREATE TABLE IF NOT EXISTS $tablename"
+        ."("
+        ."id INT NOT NULL auto_increment primary key,"
+        ."name char(32) NOT NULL,"
+        ."comment text NOT NULL,"
+        ."date  text NOT NULL"
+        .");";
+        $pdo->query($c_sql);
+        //------------------------
         echo "connection sucess<br>";
     } catch (PDOException $e) {
         echo $e-> getMessage()."<br>";
@@ -37,24 +47,24 @@
     
     
     
-    //count row
-    $c_sql ="SELECT * from $tablename";
-    $c_result = $pdo->query($c_sql);
-    $r_count = $c_result->rowCount() + 1;
-    
+    /*/count row
+     $c_sql ="SELECT * from $tablename";
+     $c_result = $pdo->query($c_sql);
+     $r_count = $c_result->rowCount() + 1;
+     */
     //check the password
     if(!empty($e_password) && $e_password=='hell'){
         //------------------------------------------logic ------------------------------//
         
         if(!empty($rw_num)&& !empty($name) && !empty($comment)){
             //edite written data
-            $sql = "update mission_4_table set name='$name', comment='$comment' where id=$rw_num";
+            $sql = "update $tablename set name='$name', comment='$comment' where id=$rw_num";
             $result = $pdo -> query($sql);
             
         }else
             //edit data
             if(!empty($e_num)){
-                $s_sql ='SELECT * from mission_4_table';
+                $s_sql ='SELECT * from mission_4_db';
                 $s_result = $pdo->query($s_sql);
                 foreach($s_result as $row){
                     if($row['id'] == $e_num){
@@ -80,10 +90,11 @@
                     if(!empty($name) && !empty($comment)){
                         echo $r_count;
                         $pdo = new PDO($dsn,$dbusername,$dbpassword);
-                        $sql = $pdo -> prepare("INSERT INTO $tablename(id,name,comment,date) VALUES(:id,:name,:comment,NOW())");
-                        $sql -> bindParam(':id',$r_count,PDO::PARAM_STR);
+                        $sql = $pdo -> prepare("INSERT INTO $tablename(name,comment,date) VALUES(:name,:comment,:date)");
+                        //$sql -> bindParam(':id',$r_count,PDO::PARAM_STR);
                         $sql -> bindParam(':name',$name,PDO::PARAM_STR);
                         $sql -> bindParam(':comment',$comment,PDO::PARAM_STR);
+                        $sql -> bindParam(':date',$date,PDO::PARAM_STR);
                         $sql -> execute();
                         echo "データ正常に更新しました";
                     }
@@ -99,20 +110,24 @@
         echo   "パースワード入力！";
     }
     function showData($pdo){
-        $s_sql ="SELECT * from mission_4_table";
+        $s_sql ="SELECT * from mission_4_db group by id";
         $s_result = $pdo->query($s_sql);
-        echo "<center>";
+        //echo "<center>";
         foreach($s_result as $row){
             echo $row['id'].'|'.$row['name'].'|'.$row['comment'].'|'.$row['date']
             ."<hr width=".'"30%"'.">";
         }
-        echo "</center>";
+        //echo "</center>";
         echo $r_count;
     }
     
     ?>
 
 
+<center>
+<table>
+<tr>
+<td bgcolor="#E7FA07" width="40%">
 <form action="" method="post">
 <input type="text" name="name" value="<?php echo $ed_name; ?>" placeholder="名前" size="20"><br>
 <input type="text" name="comment" value="<?php echo $ed_comment; ?>" placeholder="コメント" size="20"><br>
@@ -122,25 +137,30 @@
 </form>
 <hr>
 <h5>削除よする番号を入力</h5>
-<form  action="mission_4-1.php" method="post">
+<form  action="mission_4.php" method="post">
 <input type="text" name="d_name" placeholder="編集番号を入力"><br>
 <input type="password" name="password" placeholder="パースワードを入力"><br>
 <input type="submit" value="削除"><br>
 </form>
 <hr>
 <h5>編集よする番号を入力</h5>
-<form action="mission_4-1.php" method="post">
+<form action="mission_4.php" method="post">
 <input type="text" name="e_name" placeholder="削除番号を入力"><br>
 <input type="password" name="password" placeholder="パースワードを入力"><br>
 <input type="submit" value="編集">
 </form>
 <hr>
-<?php
-    //show database to the page
-    showData($pdo);
-    
-    
-    ?>
+<center>
+</td>
+
+<td bgcolor = "#07E6FA">
+<?php showData($pdo);  ?>
+
+</td>
+</tr>
+</table>
+</center>
+
 
 </body>
 </html>
